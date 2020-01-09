5cb08a27142eac29b0b02548c571986428a5b279
AladinBS <aladin.bensassi@gmail.com>
2020-01-09 22:28:00 +0000

    push

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -2,7 +2,6 @@
 class Square:
     def __init__(self, size=0):
         """Method
-		
 		"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -13,21 +12,18 @@ class Square:
 
     def area(self):
         """ Returns the square
-		
         """
         return (self.__size ** 2)
 
     @property
     def size(self):
         """ Returns the size value
-		
         """
         return self.__size
 
     @size.setter
     def size(self, value):
         """ Set the size value of the object
-		
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -38,7 +34,6 @@ class Square:
 
     def my_print(self):
         """Prints a # square
-		
         """
         if not self.__size:
             print()
