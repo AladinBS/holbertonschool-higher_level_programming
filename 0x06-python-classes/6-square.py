4e873909cf7b2199ae09d919ae3b049f5133d776
AladinBS <aladin.bensassi@gmail.com>
2020-01-09 22:47:42 +0000

    m

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 class Square:
     """ Define a square by size
-
     """
     def __init__(self, size=0, position=(0, 0)):
         """ Init the square object
@@ -28,15 +27,13 @@ class Square:
     @property
     def position(self):
         """ Position value
-        
-	"""
+        """
         return self.__position
 
     @position.setter
     def position(self, value):
         """ Value of a square object
-        
-	"""
+        """
         if type(value) is not tuple or len(value) != 2 or \
            type(value[0]) is not int or value[0] < 0 or \
            type(value[1]) is not int or value[1] < 0:
@@ -46,15 +43,13 @@ class Square:
 
     def area(self):
         """ Returns the square are of the object
-        
-	"""
+        """
         return (self.__size ** 2)
 
     def my_print(self):
         """ Print a # square
         based on the size value
-
-        """
+	"""
         if self.size == 0:
             print()
         else:
