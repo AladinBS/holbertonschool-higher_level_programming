6620b5fde348e109cfd728c9c57607126d435884
AladinBS <aladin.bensassi@gmail.com>
2020-04-02 20:59:34 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """
-states name
+States
 """
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
@@ -15,7 +14,6 @@ if __name__ == '__main__':
     FROM states \
     WHERE CONVERT(`name` USING Latin1) \
     COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
-
     states = cur.fetchall()
 
     for state in states:
